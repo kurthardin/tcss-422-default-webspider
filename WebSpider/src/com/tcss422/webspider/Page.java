81b0c197c6eb93e1d38d2591fbc1c54a12a060ff
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-04-19 20:14:05 +0000

    Added necessary instance variables to Page and refactored existing code.

--- WebSpider/src/com/tcss422/webspider/Page.java
+++ WebSpider/src/com/tcss422/webspider/Page.java
@@ -6,11 +6,18 @@ import java.util.List;
 
 public class Page {
 	
+	public final URL my_address;
 	public String content;
 	public final int [] keywordCounts;
 	public final List<URL> links;
 	
-	public Page() {
+	public int wordCount;
+	
+	public long parseStart;
+	public long parseStop;
+	
+	public Page(final URL a_url) {
+		my_address = a_url;
 		keywordCounts = new int [SpiderController.MAX_KEYWORDS];
 		for (int i = 0; i < keywordCounts.length; i++) {
 			keywordCounts[i] = 0;
--- WebSpider/src/com/tcss422/webspider/PageParserTest.java
+++ WebSpider/src/com/tcss422/webspider/PageParserTest.java
@@ -1,22 +1,33 @@
 package com.tcss422.webspider;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class PageParserTest {
 
 	/**
 	 * 
 	 */
 	public static void run() {
-		Page testPage = new Page();
-		testPage.content = "<html>\n<head>\n<title>\nJava Practices -> Parse text\n</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"../stylesheet8.css\" media=\"all\">\n<link rel=\"shortcut icon\" href='/images/favicon.ico' type=\"image/vnd.microsoft.icon\">\n<meta name=\"description\" content=\"Concise presentations of java programming practices, tasks, and conventions, amply illustrated with syntax highlighted code examples.\">\n<a href='/home/HomeAction.do' title='Table of Contents'>Home</a> |\n<div class='menu-bar'>";
-		SpiderController ctrl = new SpiderController();
-		ctrl.addKeyword("the");
-		ctrl.addKeyword("a");
-		ctrl.addKeyword("title");
-		PageParser parser = new PageParser(testPage, ctrl);
-		parser.run();
-		System.out.println("Keyword counts:");
-		for (int i = 0; i < ctrl.getKeywords().size(); i++) {
-			System.out.println("   " + ctrl.getKeywords().get(i) + ": " + testPage.keywordCounts[i]);
+		Page testPage = null;
+		try {
+			testPage = new Page(new URL("http://www.google.com/"));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (testPage != null) {
+			testPage.content = "<html>\n<head>\n<title>\nJava Practices -> Parse text\n</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"../stylesheet8.css\" media=\"all\">\n<link rel=\"shortcut icon\" href='/images/favicon.ico' type=\"image/vnd.microsoft.icon\">\n<meta name=\"description\" content=\"Concise presentations of java programming practices, tasks, and conventions, amply illustrated with syntax highlighted code examples.\">\n<a href='/home/HomeAction.do' title='Table of Contents'>Home</a> |\n<div class='menu-bar'>";
+			SpiderController ctrl = new SpiderController();
+			ctrl.addKeyword("the");
+			ctrl.addKeyword("a");
+			ctrl.addKeyword("title");
+			PageParser parser = new PageParser(testPage, ctrl);
+			parser.run();
+			System.out.println("Keyword counts:");
+			for (int i = 0; i < ctrl.getKeywords().size(); i++) {
+				System.out.println("   " + ctrl.getKeywords().get(i) + ": " + testPage.keywordCounts[i]);
+			}
 		}
 	}
 }
