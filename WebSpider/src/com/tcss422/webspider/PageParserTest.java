6e1c4e948fa466b477da917861fd82926daf8369
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-04-20 18:38:16 +0000

    [Update] Added tests for local links.

--- WebSpider/src/com/tcss422/webspider/PageParserTest.java
+++ WebSpider/src/com/tcss422/webspider/PageParserTest.java
@@ -4,14 +4,21 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class PageParserTest {
-
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PageParserTest.run();
+	}
+	
 	/**
 	 * Runs Tests on the PageParser class.
 	 */
 	public static void run() {
 		URL test_url = null;
 		try {
-			test_url = new URL("http://www.google.com/");
+			test_url = new URL("http://www.test.com/some/url/page.html");
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -19,7 +26,7 @@ public class PageParserTest {
 		
 		if (test_url != null) {
 			Page test_page = new Page(test_url);
-			test_page.my_content = "<html>\n<head>\n<title>\nJava Practices -> Parse text\n</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"../stylesheet8.css\" media=\"all\">\n<link rel=\"shortcut icon\" href='/images/favicon.ico' type=\"image/vnd.microsoft.icon\">\n<meta name=\"description\" content=\"Concise presentations of java programming practices, tasks, and conventions, amply illustrated with syntax highlighted code examples.\">\n<a href='/home/HomeAction.do' title='Table of Contents'>Home</a> |\n<div class='menu-bar'>";
+			test_page.my_content = "<html>\n<head>\n<title>\nJava Practices -> Parse text\n</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"../stylesheet8.css\" media=\"all\">\n<link rel=\"shortcut icon\" href='/images/favicon.ico' type=\"image/vnd.microsoft.icon\">\n<meta name=\"description\" content=\"Concise presentations of java programming practices, tasks, and conventions, amply illustrated with syntax highlighted code examples.\">\n<a href=\"HomeAction.do\">Home</a>\n<a href='./home/HomeAction.do' title='Table of Contents'>Home</a>\n<a href='../../home/HomeAction.do' title='Table of Contents'>Home</a>\n<a href='../home/HomeAction.do' title='Table of Contents'>Home</a>\n<a href='/home/HomeAction.do' title='Table of Contents'>Home</a> |\n<div class='menu-bar'>";
 			SpiderController ctrl = new SpiderController(test_url);
 			ctrl.addKeyword("the");
 			ctrl.addKeyword("a");
