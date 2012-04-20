15c96745074b54f9679e2c4b8e9eb34e608c21bc
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-04-20 18:37:19 +0000

    [Update] Local links now get fixed correctly.

--- WebSpider/src/com/tcss422/webspider/PageParser.java
+++ WebSpider/src/com/tcss422/webspider/PageParser.java
@@ -38,29 +38,46 @@ public class PageParser implements Runnable {
 	 */
 	@Override
 	public void run() {
-		Pattern ptrn = Pattern.compile("a href=\\'\\S*|\\w*");
-		Matcher matcher = ptrn.matcher(my_page.my_content);
+		Pattern pattern = Pattern.compile("a\\shref=[\\'\\\"]\\S+[\\'\\\"]|\\w+");
+		Matcher matcher = pattern.matcher(my_page.my_content);
 		my_page.my_parse_start = System.nanoTime();
 		while (matcher.find()) {
 			String match = matcher.group();
-			if (match.length() > 0) {
-				System.out.println(match);
-				if (match.startsWith("a href")) {
-					String urlStr = match.substring(8, match.length() - 1);
-					System.out.println("   url: " + urlStr);
-					
-					// TODO: Check for local links
-					
-					try {
-						URL url = new URL(urlStr);
-						my_page.my_links.add(url);
-					} catch (MalformedURLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+			System.out.println(match);
+
+			if (match.startsWith("a href")) {									// Found a link
+				String url_str = match.substring(8, match.length() - 1);
+
+				if (!url_str.startsWith("http://")) {							// Fix url for local links 
+					String url_str_prefix = my_page.my_address.toString();
+					url_str_prefix = url_str_prefix.substring(
+							0, url_str_prefix.lastIndexOf("/"));
+
+					if (url_str.startsWith("./")) {
+						url_str = url_str.substring(2);
+					} else if (url_str.startsWith("/")) {
+						url_str = url_str.substring(1);
+					} else {
+						while(url_str.startsWith("../")) {
+							url_str = url_str.substring(3);
+							url_str_prefix = url_str_prefix.substring(
+									0, url_str_prefix.lastIndexOf("/"));
+						}
 					}
-				} else {
-					processWord(match);
+					url_str = url_str_prefix + "/" + url_str;
 				}
+
+				System.out.println("   url: " + url_str);
+				try {
+					URL url = new URL(url_str);
+					my_page.my_links.add(url);
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			} else {															// Found a word
+				processWord(match);
 			}
 		}
 		my_page.my_parse_stop = System.nanoTime();
