bed00e943da322aa91dd14c766ba2ed416adb271
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-04-20 16:51:31 +0000

    Added stub for PageRetriever class.

---
+++ WebSpider/src/com/tcss422/webspider/PageRetriever.java
@@ -0,0 +1,17 @@
+package com.tcss422.webspider;
+
+import java.net.URL;
+
+public class PageRetriever implements Runnable {
+	
+	public PageRetriever(final URL a_url, final SpiderController aController) {
+		// TODO Implement PageRetriever constructor
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
