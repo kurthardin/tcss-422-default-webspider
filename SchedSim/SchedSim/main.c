27637f6730e0c429198df6a3e6430e128d81ed16
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-05-09 17:27:36 +0000

    Setup Xcode project.  (If not using Xcode just work directly with the source files in .../SchedSim/SchedSim)

---
+++ SchedSim/SchedSim/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SchedSim
+//
+//  Created by Kurt Hardin on 5/9/12.
+//  Copyright (c) 2012 University of Washington at Tacoma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
