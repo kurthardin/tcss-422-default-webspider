cc06974541e124db1b2ab04d4a0afa33e03670be
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-05-17 19:56:33 +0000

    Started IODevice implementation.

---
+++ SchedSim/IODevice.c
@@ -0,0 +1,27 @@
+//
+//  IODevice.c
+//  SchedSim
+//
+//  Created by Kurt Hardin on 5/17/12.
+//  Copyright (c) 2012 University of Washington at Tacoma. All rights reserved.
+//
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "IODevice.h"
+
+void *run(void *);
+
+pthread_t * io_device_init(char * type) {
+    pthread_t *thread_id = malloc(sizeof(pthread_t));
+    pthread_create(thread_id, NULL, run, (void *)type);
+    return thread_id;
+}
+
+void *run(void * type) {
+    printf("%s device running", (char *)type);
+    sleep(500); // TODO set randomly
+    // TODO Generate interrupt
+    return EXIT_SUCCESS;
+}
--- SchedSim/SchedSim/IODevice.h
+++ SchedSim/SchedSim/IODevice.h
@@ -6,9 +6,11 @@
 //  Copyright (c) 2012 University of Washington at Tacoma. All rights reserved.
 //
 
+#include <pthread.h>
+
 #ifndef SchedSim_IODevice_h
 #define SchedSim_IODevice_h
 
-
+pthread_t * io_device_init(char *);
 
 #endif
