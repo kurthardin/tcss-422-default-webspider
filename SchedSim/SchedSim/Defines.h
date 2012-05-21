d5278762e3b31f7d4843247d97eb736618a8b452
kdhardin@uw.edu <kdhardin@uw.edu@cca002be-d877-9eb5-34f2-8755f0651649>
2012-05-21 16:54:57 +0000

    Added file for defining useful macro (e.g. boolean type and YES/NO, TRUE/FALSE)

---
+++ SchedSim/SchedSim/Defines.h
@@ -0,0 +1,19 @@
+//
+//  Defines.h
+//  SchedSim
+//
+//  Created by Kurt Hardin on 5/21/12.
+//  Copyright (c) 2012 University of Washington at Tacoma. All rights reserved.
+//
+
+#ifndef SchedSim_Defines_h
+#define SchedSim_Defines_h
+
+typedef int boolean;
+
+#define YES     1
+#define NO      0
+#define TRUE    1
+#define FALSE   0
+
+#endif
